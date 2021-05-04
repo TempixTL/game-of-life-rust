ceba47b6a288a276950718939caa764971e62a1a
Thomas Lauerman <tom@lauerman.dev>
2021-05-03 23:50:57 -0500

    Parse initial board from Config

--- src/lib.rs
+++ src/lib.rs
@@ -30,3 +30,65 @@ pub mod cfg {
         StepCountError { found: Option<i32> }
     }
 }
+
+pub mod engine {
+    use std::fmt;
+    use std::fmt::Formatter;
+    use crate::cfg::Config;
+
+    pub fn run(config: Config) {
+        let Config { starting_board, steps } = config;
+        match Board::new(starting_board) {
+            Some(board) => {
+                println!("Starting board:\n{}", board);
+            },
+            None => eprintln!("Unable to parse board."),
+        }
+    }
+
+    enum Cell {
+        Alive,
+        Dead,
+    }
+
+    /// A Board which stores a grid of [`Cell`] in row-major order.
+    struct Board {
+        grid: Vec<Vec<Cell>>,
+    }
+
+    impl Board {
+        pub fn new(str: String) -> Option<Board> {
+            let mut str_iter = str.lines();
+
+            let grid_size_str = str_iter.next()?;
+            let grid_size = grid_size_str.parse::<usize>().ok()?;
+            let grid: Vec<Vec<Cell>> = str_iter.map(|grid_line|
+                grid_line.split(" ").map(|grid_char| match grid_char {
+                    "1" => Cell::Alive,
+                    _   => Cell::Dead,
+                }).collect()
+            ).collect();
+
+            if grid_size == grid.len() && grid.iter().all(|grid_line| grid_size == grid_line.len()) {
+                Some(Board { grid })
+            } else {
+                None
+            }
+        }
+    }
+
+    impl fmt::Display for Board {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            for grid_line in self.grid.iter() {
+                let grid_line_str = grid_line.iter().map(|cell| match cell {
+                    Cell::Alive => "1".to_string(),
+                    Cell::Dead  => ".".to_string(),
+                }).collect::<Vec<String>>().join(" ");
+                
+                writeln!(f, "{}", grid_line_str)?
+            }
+            
+            Ok(())
+        }
+    }
+}
