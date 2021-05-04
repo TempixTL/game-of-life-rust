16086a4479f31dc76ca56d8d19e54f7581d4847c
Thomas Lauerman <tom@lauerman.dev>
2021-05-03 19:12:28 -0500

    Organize imports

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 use std::env;
-use game_of_life::cfg::Config;
-use game_of_life::cfg;
+use game_of_life::cfg::{Config, ConfigError};
+use game_of_life::engine;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args);
 
     match config {
-        Ok(config) => println!("{:?}", config.steps),
-        Err(cfg::ConfigError::ArgumentCountError { found, expected }) =>
+        Ok(config) => engine::run(config),
+        Err(ConfigError::ArgumentCountError { found, expected }) =>
             eprintln!("Invalid number of arguments. Found {}, expected {}", found, expected),
-        Err(cfg::ConfigError::StepCountError { found: Some(steps) }) =>
+        Err(ConfigError::StepCountError { found: Some(steps) }) =>
             eprintln!("Invalid step number. Found {}", steps),
-        Err(cfg::ConfigError::StepCountError { found: None }) =>
+        Err(ConfigError::StepCountError { found: None }) =>
             eprintln!("Invalid step number."),
-        Err(cfg::ConfigError::FileAccessError { file_name }) =>
+        Err(ConfigError::FileAccessError { file_name }) =>
             eprintln!("Unable to open file '{}'.", file_name),
     }
 }
