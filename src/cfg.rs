2b2254d13a9563dc39a508ec1a5dd9543f8326a4
Thomas Lauerman <tom@lauerman.dev>
2021-05-04 20:45:26 -0500

    Add unit tests for cfg module

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,5 +1,6 @@
 use std::fs;
 
+#[derive(Debug)]
 pub struct Config {
     pub starting_board: String,
     pub steps: i32,
@@ -23,8 +24,51 @@ impl Config {
     }
 }
 
+#[derive(Debug)]
 pub enum ConfigError {
     ArgumentCountError { found: usize, expected: usize },
     FileAccessError { file_name: String },
     StepCountError { found: Option<i32> }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_should_argument_count_error() {
+        let args = [];
+        match Config::new(&args) {
+            Err(ConfigError::ArgumentCountError { found: 0, expected: _ }) => (),
+            _ => panic!(),
+        }
+    }
+
+    #[test]
+    fn test_should_file_access_error() {
+        let args = ["exe".to_string(), "/inaccessible_file".to_string(), "0".to_string()];
+        match Config::new(&args) {
+            Err(ConfigError::FileAccessError{ file_name: _ }) => (),
+            _ => panic!(),
+        }
+    }
+
+    #[test]
+    fn test_should_step_count_error() {
+        let args = ["exe".to_string(), "input/game-of-life-in1.txt".to_string(), "-1".to_string()];
+        match Config::new(&args) {
+            Err(ConfigError::StepCountError { found: _ }) => (),
+            _ => panic!(),
+        }
+    }
+
+    #[test]
+    fn test_should_return_config() -> Result<(), ConfigError> {
+        let args = ["exe".to_string(), "input/game-of-life-in1.txt".to_string(), "3".to_string()];
+        let config = Config::new(&args)?;
+
+        assert_ne!(config.starting_board.len(), 0);
+        assert_eq!(config.steps, 3);
+        Ok(())
+    }
 }
