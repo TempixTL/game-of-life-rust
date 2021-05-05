176919e742c72035bb014b043c89fcbf8d62a40e
Thomas Lauerman <tom@lauerman.dev>
2021-05-04 21:35:15 -0500

    Add docs to cfg

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,12 +1,41 @@
 use std::fs;
 
+/// A struct representing the startup configuration for the applicaiton.
 #[derive(Debug)]
 pub struct Config {
+    /// A string representation of the initial Game of Life board.
     pub starting_board: String,
+    /// The number of iterations the `starting_board` should go through.
     pub steps: i32,
 }
 
 impl Config {
+
+    /// Takes command-line arguments and parses them into a [Config].
+    /// 
+    /// # Arguments
+    /// * `args` - Arguments from the command-line.
+    /// 
+    /// # Returns
+    /// 
+    /// A [Config] if successful, or a [ConfigError] with the cause of the
+    /// failure.
+    /// 
+    /// ```
+    /// use game_of_life::cfg::{Config, ConfigError};
+    /// 
+    /// # fn main() -> Result<(), ConfigError> {
+    /// let args = [
+    ///     "target/debug/game-of-life".to_string(),
+    ///     "input/game-of-life-in1.txt".to_string(),
+    ///     "3".to_string()
+    /// ];
+    /// 
+    /// let config = Config::new(&args)?;
+    /// assert_eq!(config.steps, 3);
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn new(args: &[String]) -> Result<Config, ConfigError> {
         if args.len() != 3 {
             return Err(ConfigError::ArgumentCountError { found: args.len(), expected: 3 })
@@ -24,10 +53,19 @@ impl Config {
     }
 }
 
+/// An error which has occurred while creating a [Config].
 #[derive(Debug)]
 pub enum ConfigError {
+    /// An error which occurs when the number of arguments passed to
+    /// `Config::new` was incorrect. Contains both the `found` and `expected`
+    /// number of arguments.
     ArgumentCountError { found: usize, expected: usize },
+    /// An error which occurs when the specified file is not readable for some
+    /// reason. Contains the `file_name` tried.
     FileAccessError { file_name: String },
+    /// An error which occurs when the number of specified iterations of the
+    /// board is invalid. Contains the number of iterations `found`, if a valid
+    /// number at all.
     StepCountError { found: Option<i32> }
 }
 
