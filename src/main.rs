d6f9f062e087ccda186967323b671fe9def67b36
Thomas Lauerman <tom@lauerman.dev>
2021-05-07 13:02:49 -0500

    Refactor error printouts

--- src/main.rs
+++ src/main.rs
@@ -9,31 +9,35 @@ fn main() {
     match config {
         Ok(config) => engine::run(config),
         Err(ConfigError::ArgumentCountError { found, expected }) => {
-            eprintln!("Invalid number of arguments. Found {}, expected {}", found, expected);
-            print_help(&args[0]);
+            print_help(
+                &format!("Invalid number of arguments. Found {}, expected {}", found, expected),
+                &args[0]);
         },
         Err(ConfigError::StepCountError { found: Some(steps) }) => {
-            eprintln!("Invalid step number. Found {}", steps);
-            print_help(&args[0]);
+            print_help(
+                &format!("Invalid step number. Found {}", steps),
+                &args[0]);
         },
         Err(ConfigError::StepCountError { found: None }) => {
-            eprintln!("Invalid step number.");
-            print_help(&args[0]);
+            print_help(
+                &format!("Invalid step number."),
+                &args[0]);
         },
         Err(ConfigError::FileAccessError { file_name }) => {
-            eprintln!("Unable to open file '{}'.", file_name);
-            print_help(&args[0]);
+            print_help(
+                &format!("Unable to open file '{}'.", file_name),
+                &args[0]);
         },
     }
 }
 
-fn print_help(prog_name: &str) {
-    eprintln!("\
+fn print_help(err_msg: &str, prog_name: &str) {
+    eprintln!("{}
 Simulates Conway's Game of Life.
 
 Syntax: {} INIT_BOARD_FILE STEP_NUMBER
 Parameters:
 INIT_BOARD_FILE - Path to the initial Game of Life board.
 STEP_NUMBER - Number of steps to iterate. Should be a positive number.",
-    prog_name);
+    err_msg, prog_name);
 }
